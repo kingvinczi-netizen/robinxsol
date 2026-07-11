a42c5071a3bdcef5dffa05ba6bb044f196c4115d
Ezeala victory <kingvinczi@gmail.com>
2026-07-11 19:01:15 +0100

    Add WalletConnect project id for wallet connect

--- dapp/lib/wagmi.ts
+++ dapp/lib/wagmi.ts
@@ -5,7 +5,8 @@ import { base, sepolia } from "wagmi/chains";
 export const config = getDefaultConfig({
   appName: "ROBINXSOL Dapp",
   projectId:
-    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "PLACEHOLDER_PROJECT_ID",
+    process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ||
+    "a9a4dd736eb4ab008da4aa7c3bb846d1",
   chains: [sepolia, base],
   ssr: true,
 });
